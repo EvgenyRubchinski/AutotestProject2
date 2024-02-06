2fc9628d7d493322581a2ad836aaa842f184cbe9
Ruban_Test <e.ruban@pflb.ru>
2024-02-06 22:52:08 +0300

    commit update

---
+++ src/main/java/MyFirstProject.java
@@ -0,0 +1,8 @@
+public class MyFirstProject {
+    //наименование класса пишется с большой буквы
+    public static void main(String[] args){
+
+        System.out.println("Hello world!");
+
+    }
+}
---
+++ src/main/java/VariablesExample.java
@@ -0,0 +1,18 @@
+public class VariablesExample {
+    public static void main(String[] args){
+        int x; // объявление переменной
+        x = 10; // присвоение переменной
+        System.out.println(x); // в консоли выведется 10
+
+        int y = 10; // объявили переменную и сразу присвоили значение
+        System.out.println(y); // выведется 10
+
+        int a, b;
+        a = 10;
+        b = 20;
+        System.out.println(a); // 10
+        System.out.println(b); // 20
+        
+
+    }
+}
